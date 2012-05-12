8490d08cf64e86a6d9b6e69d682bf37789ca2767
laputa <ye.laputa@gmail.com>
2012-05-12 10:53:53 +0800

    show more, use last result

--- parse.py
+++ parse.py
@@ -1,12 +1,9 @@
 # coding: utf8
 # laputa
 
-def parse_langdao(text, maxline=None):
+def parse_langdao(text):
     items = text.split('\n')
     if items[0].startswith('*'):
         items[0] = items[0][1:]
-    if maxline and len(items) > maxline:
-        items = items[:maxline]
-        items.append('more..')
     items = [i.decode('utf8') for i in items]
     return items
--- translate.py
+++ translate.py
@@ -12,34 +12,36 @@ setting = sublime.load_settings('translate.sublime-settings')
 pkgpath = sublime.packages_path()
 dicpath = os.path.join(pkgpath, setting.get('dictdir'))
 dic     = stardict.Dict(dicpath)
-maxline = setting.get('maxline', 5)
-maxline = maxline if maxline > 0 else 5
 history = setting.get('history', False)
 
 
 class LocalfileTranslateCommand(sublime_plugin.TextCommand):
-    def show(self, alist):
-        self.view.window().show_quick_panel(alist, self.on_done)
+    def show(self, items, count=None):
+        if count and len(items) > count:
+            items = items[:count]
+            items.append('more..')
+        self.view.window().show_quick_panel(items, self.on_done)
 
     def on_done(self, arg):
         if arg != -1:
             if arg == maxline:
-                res = self.query(self.symbol)
-                self.show(res)
+                self.show(self.tetc)
             elif arg == maxline + 1:
                 pass
         else:
             pass
 
     def run(self, edit):
+        maxline = setting.get('maxline', 4)
+        maxline = maxline if maxline > 0 else 4
         region = self.view.word(self.view.sel()[0])
         self.symbol = self.view.substr(region).lower()
-        res = self.query(self.symbol, maxline)
-        self.show(res)
+        self.tetc = self.query(self.symbol)
+        self.show(self.tetc, maxline)
 
-    def query(self, word, maxline=None):
+    def query(self, word):
         text = dic.query(word)
-        res = parse.parse_langdao(text, maxline)
+        res = parse.parse_langdao(text)
         return res
 
 class NetworkTranslateCommand(LocalfileTranslateCommand):
